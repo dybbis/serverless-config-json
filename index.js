f85f849539a93caad992208d421f214b92545135
Jo Dybdal <jdy@ambita.com>
2021-01-22 09:55:01 +0100

    Init

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+
+class ConfigJson {
+
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options || {};
+    this.commands = {
+      cloudfrontInvalidate: {
+        usage: "Copy config.json to folder",
+        lifecycleEvents: [
+          'copy'
+        ]
+      }
+    };
+
+    this.hooks['after:deploy:deploy'] = this.copy.bind(this);
+  }
+
+  copy() {
+    const { cli } = this.serverless;
+    const { from, to } = this.serverless.service.custom.configJson;
+    const { stage } = this.serverless.service.provider;
+
+    if (!from || !to) {
+      cli.consoleLog('Missing configuration, check from and to!');
+
+      return;
+    }
+
+    if (!stage) {
+      cli.consoleLog('Stage is not set, skipping copy of config.json');
+
+      return;
+    }
+
+    from = from.replace(/\/$/, "");
+    to = to.replace(/\/$/, "");
+
+    const configFile = `./${from}/${stage}.json`
+    const outputFile = `./${to}/config.json`
+
+    try {
+      fs.copyFileSync(configFile, outputFile);
+    } catch (error) {
+      cli.consoleLog(error.getMessage());
+    }
+  }
+}
+
+module.exports = CloudfrontInvalidate;
